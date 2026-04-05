d1cdd7b5a1298f1f355db4ff555a9ee23126ad1d
Rishi2553 <rishipanneerselvam@mail.com>
2026-04-06 01:25:13 +0530

    git inti

---
+++ src/main/java/com/example/conductor_service/ConductorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.conductor_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConductorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConductorServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/conductor_service/ConductorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.conductor_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConductorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
